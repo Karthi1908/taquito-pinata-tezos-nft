586e03d0142a6529edba223ab9cf2392bbffe702
Karthik <33004854+Karthi1908@users.noreply.github.com>
2021-11-28 12:53:02 +0800

    Update PinataKeys.ts

--- backend/src/PinataKeys.ts
+++ backend/src/PinataKeys.ts
@@ -1,4 +1,4 @@
 export default {
-  apiKey: "DUMMY_KEY",
-  apiSecret: "DUMMY_SECRET"
+  apiKey: "f859cdf9fa544c7aff97",
+  apiSecret: "55c27a80fd7a5145abcee89c3b8cfd4dbd7947c1193d095d8a98b48101fa2195"
 };
